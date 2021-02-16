6990dc8a6971d30b55758515b1f5253d60f85075
jbbsd1 <julien4915@hotmail.com>
2021-02-16 13:54:49 +0100

    Level 2 complete

---
+++ Assets/scripts/DontDestroyOnLoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoadScene : MonoBehaviour
+{
+
+
+    public GameObject[] objects;
+
+    void Awake()
+    {
+        foreach (var element in objects)
+        {
+            DontDestroyOnLoad(element);
+        }
+    }
+
+}
---
+++ Assets/scripts/LoadSpecificScene.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter()
+    {
+            SceneManager.LoadScene(sceneName);
+    }
+
+}
+ 
