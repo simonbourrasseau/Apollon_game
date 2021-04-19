8e9a2c6535582713bf44ed5ffe27023d1eba5812
Julien <julien4915@hotmail.com>
2021-04-19 11:58:05 +0100

    Add Retry
    Prefab pour le retry, retry sur tout les levels

---
+++ Assets/Scripts/Retry.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
